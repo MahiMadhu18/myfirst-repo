72aae44c7f06a20bc6615872bc91d5ef5285cf52
MahiMadhu18 <65780956+MahiMadhu18@users.noreply.github.com>
2020-06-01 18:44:27 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -5,3 +5,20 @@ if count = incorrect
 echo "guess was either too low or too high"
 else 
 echo "congratulated the guess is correct"
+function addseq2 {
+  local sum=0
+
+  for element in $@
+  do
+    let sum=sum+$element
+  done
+
+  echo $sum
+}
+count=3
+
+while [[ $count -gt 0 ]]
+do
+  echo "count is equal to $count"
+  let count=$count+1              # We only changed this line!
+done
