12b16ad0f88d2a07e4d3c24d80e6d4f16dc7eaa3
MahiMadhu18 <65780956+MahiMadhu18@users.noreply.github.com>
2020-06-05 15:43:36 +0530

    Update makefile.sh

--- makefile.sh
+++ makefile.sh
@@ -1,5 +1,5 @@
 touch README.md
-echo "# Guessing Game # ## by Madhu Gaddam  ##" > README.md
+echo "# myfirst-repo # ## by Madhu Gaddam  ##" > README.md
 echo "- *makefile.sh executed / README.md Made on:* $(stat -c %x makefile.sh)" >> README.md
 echo -e "- *lines in guessinggame.sh* : **$(wc -l guessinggame.sh )** \n" >> README.md
 echo "1. preview of guessinggame.sh :" >> README.md
